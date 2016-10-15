c1c93a32f27b96da4c5336ca2c86e52397a3f361
Mohammed Aizaaz Ali <mohammedaizaaz@gmail.com>
2016-10-15 14:00:25 -0400

    Update AESCBC.py

--- Ciphertext_Bit_Modified/AESCBC.py
+++ Ciphertext_Bit_Modified/AESCBC.py
@@ -7,7 +7,6 @@ from Crypto.Cipher import AES
 
 # Encryption
 aes_enc = AES.new('This key is used',AES.MODE_CBC,'This is the IV12')
-
 block_text="first block1 ecbsecond block ecbfirst block1 ecbfourth block ecbfirst block1 ecb"
 #Convert to cipher text
 cipher_text = aes_enc.encrypt(block_text)
@@ -27,7 +26,7 @@ print(plain_text)
 
 # Decryption after modifying firstbit of second block of ciphertext
 cipher_text_new='3a0cb825e1fb99f5f12d1c4c2e2b3cab12f094233b296035a76832e4e1303936ce5de6faa4460b0ca767191177ec155e6d5e7130e2a60564ed2f79494c4d1176e03e4cbc9913a084653a9b6a9febefce'
-#Decode to hex
+#Decode hex format
 decoded_cipher=cipher_text_new.decode('hex')
 #Convert to plaintext
 plain_text_new = aes_dec.decrypt(decoded_cipher)
