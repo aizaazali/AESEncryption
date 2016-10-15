a6b1ac9e13df7e50f216d1539b1855b5a8e02c17
Mohammed Aizaaz Ali <mohammedaizaaz@gmail.com>
2016-10-15 14:02:11 -0400

    Update AESOFB.py

--- Ciphertext_Bit_Modified/AESOFB.py
+++ Ciphertext_Bit_Modified/AESOFB.py
@@ -7,7 +7,6 @@ from Crypto.Cipher import AES
 
 # Encryption
 aes_enc = AES.new('This key is used',AES.MODE_OFB,'This is the IV12')
-
 block_text="first block1 ecbsecond block ecbfirst block1 ecbfourth block ecbfirst block1 ecb"
 #Convert to cipher text
 cipher_text = aes_enc.encrypt(block_text)
@@ -27,7 +26,7 @@ print(plain_text)
 
 # Decryption after modifying firstbit of second block of ciphertext
 cipher_text_new='2bfa77a98058f7c3dc7080a9165e24814617e791c67e93fae7ff1ccf5a185f2fc1910d8bc93019ddd4e1b9a30e314d408294f89ffd045b6d31b8850869b75f024b5953a23841e38af913772ebfd8fe76'
-#Decode to hex format
+#Decode hex format
 decoded_cipher=cipher_text_new.decode('hex')
 #Convert to plaintext
 plain_text_new = aes_dec.decrypt(decoded_cipher)
